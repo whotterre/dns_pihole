cfffeab9e5815304f1419aaabfcf1dc3a14da2e6
whotterre <ijeddy5.0@gmail.com>
2026-07-10 20:08:23 +0100

    style: minor cosmetic changes :)

--- dns.go
+++ dns.go
@@ -12,12 +12,12 @@ func ParseDNSHeader(data []byte) entities.DNSHeader {
 		return entities.DNSHeader{}
 	}
 	return entities.DNSHeader{
-		ID:      uint16(data[0])<<8 | uint16(data[1]),
-		Flags:   uint16(data[2])<<8 | uint16(data[3]),
-		QdCount: uint16(data[4])<<8 | uint16(data[5]),
-		AnCount: uint16(data[6])<<8 | uint16(data[7]),
-		NsCount: uint16(data[8])<<8 | uint16(data[9]),
-		ArCount: uint16(data[10])<<8 | uint16(data[11]),
+		ID:      uint16(data[0]) << 8 | uint16(data[1]),
+		Flags:   uint16(data[2]) << 8 | uint16(data[3]),
+		QdCount: uint16(data[4]) << 8 | uint16(data[5]),
+		AnCount: uint16(data[6]) << 8 | uint16(data[7]),
+		NsCount: uint16(data[8]) << 8 | uint16(data[9]),
+		ArCount: uint16(data[10]) << 8 | uint16(data[11]),
 	}
 }
 
@@ -51,12 +51,12 @@ func parseDomainNameHelper(data []byte, offset int, visited map[int]bool) (strin
 		}
 
 		// Handle DNS compression pointers (0xC0)
-		if length&0xC0 == 0xC0 {
+		if length & 0xC0 == 0xC0 {
 			if offset+1 >= len(data) {
 				break
 			}
 			// Pointer to another location (14-bit offset)
-			pointer := int(data[offset]&0x3F)<<8 | int(data[offset+1])
+			pointer := int(data[offset] & 0x3F) << 8 | int(data[offset + 1])
 			if pointer >= len(data) {
 				break
 			}
